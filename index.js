4ad3faeb9f6d0b895dcffa7fdacdddca14c702b3
Angela Vawser <amvawser@gmail.com>
2019-10-29 14:41:38 -0700

    trying to fix

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
 const arg = process.argv;
-const folder = arg[arg.length - 1];
+const path = arg[arg.length - 1];
 const fs = require('fs');
 
-if(folder !== '.') fs.mkdirSync(folder);
-fs.mkdirSync(`${folder}/src`);
+if(path !== '.') fs.mkdirSync(path);
+fs.mkdirSync(`${path}/src`);
 
-const packageJsonWriter = require('./writers/package-json-writer');
-const eslintWriter = require('./writers/eslint-writer');
-const gitignoreWriter = require('./writers/gitignore-writer');
-const travisWriter = require('./writers/travis-writer');
-const babelWrtier = require('./writers/babel-writer');
-const webpackWriter = require('./writers/webpack-writer');
-const indexWriter = require('./writers/src-index-writer');
+const createPackageJson = require('./writers/package-json-writer');
+const createEslint = require('./writers/eslint-writer');
+const createGitignore = require('./writers/gitignore-writer');
+const createTravis = require('./writers/travis-writer');
+const createBabel = require('./writers/babel-writer');
+const createWebpack = require('./writers/webpack-writer');
+const createIndex = require('./writers/src-index-writer');
 const devDependencyInstaller = require('./dependencies/dev-dependencies-installer');
 const dependencyInstaller = require('./dependencies/dependencies-installer');
 
-packageJsonWriter(folder);
-eslintWriter(folder);
-gitignoreWriter(folder);
-travisWriter(folder);
-babelWrtier(folder);
-webpackWriter(folder);
-indexWriter(folder);
-devDependencyInstaller(folder);
-dependencyInstaller(folder);
+createPackageJson(path);
+createEslint(path);
+createGitignore(path);
+createTravis(path);
+createBabel(path);
+createWebpack(path);
+createIndex(path);
+devDependencyInstaller(path);
+dependencyInstaller(path);
